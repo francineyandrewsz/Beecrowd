fe4b2af1eab985b1988a2e0810332a4694df5586
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-07-13 10:55:39 -0300

    1007 - Diferença

---
+++ diferenca.py
@@ -0,0 +1,11 @@
+#Leia quatro valores inteiros A, B, C e D
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+#Calcule e mostra a diferença do produto de A e B pelo produto de C e D
+dif = (a*b-c*d)
+
+#Imprima a mensagem DIFERENCA com todas as letras maiúsculas
+print(f'DIFERENCA = {dif}')
