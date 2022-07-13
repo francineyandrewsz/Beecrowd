2e9a1e99caf6a21a96e0d86ac3d1e1925429f21d
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-07-13 12:11:27 -0300

    Esfera

---
+++ BEE_1011.py
@@ -0,0 +1,10 @@
+#Leia o valor de seu raio
+R = int(input())
+#Considere para pi o valor 3.14159
+pi = 3.14159
+
+#Calcule o volume da esfera com a seguinte fórmula
+f = (4/3)*pi*R**3
+
+#Imprimir uma mensagem "VOLUME" com o resultado apresentado com 3 casas decimais após o ponto
+print(f'VOLUME = {f:.3f}')
