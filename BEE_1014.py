82ec0f8161015a538149db14172b2f31744d4290
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-07-15 18:02:44 -0300

    Consumo

---
+++ BEE_1014.py
@@ -0,0 +1,7 @@
+#O valor inteiro da distância percorrida em km
+x = int(input())
+#O valor real de combustível gasto
+y = float(input())
+
+#Imprima o valor do consumo médio do automóvel com 3 casas após a vírgula
+print(f'{x/y:.3f} km/l')
